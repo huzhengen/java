321d67e30ad597e727cb264b34765e390cf7b80c
卢方 <huzhengen@163.com>
2018-04-14 22:34:07 +0800

    java_xdml

---
+++ java_xdml/src/com/company/Cal2.java
@@ -0,0 +1,12 @@
+package com.company;
+
+//用来定义一个多项式，求解
+//a*x + b*y
+public class Cal2 {
+    public int a;
+    public int b;
+
+    public int result(int x, int y){
+        return a*x + b*y;
+    }
+}
