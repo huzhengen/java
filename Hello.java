ba70bdb7634c633b3da8e82adbe4685c09692b9c
hukai <huzhengen@163.com>
2019-07-05 16:53:46 +0800

    learn java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+        int x = 133;
+        System.out.println(x);
+
+        x = 899;
+        System.out.println(x);
+//        System.out.println("hhello world");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# java
+# Java
+
+在Java中，变量分为两种：基本类型的变量和引用类型的变量。
+
+int x = 1;
+
+基本数据类型是CPU可以直接进行运算的类型。Java定义了以下几种基本数据类型：
+
+* 整数类型：byte，short，int，long
+
+* 浮点数类型：float，double
+
+* 字符类型：char
+
+* 布尔类型：boolean
+
+Java里字符串是引用类型。
