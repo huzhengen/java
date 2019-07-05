761e1e3583158215ebb2f86cbda708b289afe1e4
hukai <huzhengen@163.com>
2019-07-05 17:22:48 +0800

    Demo1.java

---
+++ Demo1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int ns[] = {1,23,4,5,67,8};
+        System.out.println(Arrays.toString(ns));
+    }
+}
