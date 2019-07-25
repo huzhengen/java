b585a0ea75b90da0b4336ba7fcfe5bf018277465
hukai <huzhengen@163.com>
2019-07-25 10:32:44 +0800

    Person.java

---
+++ Book.java
@@ -0,0 +1,6 @@
+class Book{
+	public String name;
+	public String author;
+	public String isbn;
+	public double price;
+}
---
+++ Person.java
@@ -0,0 +1,14 @@
+class Person{
+	public String name;
+	public int age;
+}
+
+Person ming = new Person();
+
+ming.name = "xiao ming";
+ming.age = 22;
+System.out.println(ming.name);
+
+Person hong = new Person();
+hong.name = "xiao hong";
+hong.age = 33;
